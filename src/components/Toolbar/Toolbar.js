e5f67f17a52bba7af9d309df1f185ab84115fc06
Karolina kosiorowska <kaarolina.kosiorowska@gmail.com>
2019-05-24 23:30:18 +0200

    Initial commit and added toolbar

---
+++ src/components/Toolbar/Toolbar.js
@@ -0,0 +1,24 @@
+import React from 'react';
+
+import './Toolbar.css';
+import Bug from './../../images/bug.png';
+import { Container, Navbar, Nav, Form, Button, FormControl } from 'react-bootstrap';
+
+const toolbar = (props) => (
+    <div className="Toolbar">
+      <Navbar bg="dark" variant="dark">
+        <Navbar.Brand href="#ruleMenagment"><img src={Bug}/></Navbar.Brand>
+        <Nav className="mr-auto">
+          <Nav.Link href="http://localhost:2990/jira/secure/Dashboard.jspa">Back to Jira</Nav.Link>
+          <Nav.Link href="#ruleMenagment">Rule menagment</Nav.Link>
+          <Nav.Link href="#logs">Logs</Nav.Link>
+        </Nav>
+        <Form inline>
+          <FormControl type="text" placeholder="Search" className="mr-sm-2" />
+          <Button variant="outline-info">Search</Button>
+        </Form>
+      </Navbar>
+    </div>
+);
+
+export default toolbar;
