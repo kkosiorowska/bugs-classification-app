5b953d666175da60cdab4fffa725ce4cfc72f2d8
Karolina kosiorowska <kaarolina.kosiorowska@gmail.com>
2019-06-08 11:52:52 +0200

    Delete Logs

--- src/components/Toolbar/Toolbar.js
+++ src/components/Toolbar/Toolbar.js
@@ -10,8 +10,8 @@ const toolbar = (props) => (
         <Navbar.Brand href="#ruleMenagment"><img src={logo} alt="logo"/></Navbar.Brand>
         <Nav className="mr-auto">
           <Nav.Link href="http://localhost:2990/jira/secure/Dashboard.jspa">Back to Jira</Nav.Link>
-          <Nav.Link href="#ruleMenagment">Rule menagment</Nav.Link>
-          <Nav.Link href="#logs">Logs</Nav.Link>
+          <Nav.Link href="#ruleMenagment" active>Rule menagment</Nav.Link>
+          {/* <Nav.Link href="#logs">Logs</Nav.Link> */}
         </Nav>
         <Form inline>
           <FormControl type="text" placeholder="Search" className="mr-sm-2" />
